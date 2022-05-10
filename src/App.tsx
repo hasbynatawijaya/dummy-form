6c58b5f36875c9480cfd784fab194bdd9053384a
kalibanip <kautzar.alibani@postpay.io>
2022-05-10 09:52:57 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		window.close();
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<form onSubmit={handleSubmit}>
+					<input type="text" placeholder="Input 1" />
+					<input type="text" placeholder="Input 2" />
+					<input type="text" placeholder="Input 3" />
+					<button type="submit">Submit</button>
+				</form>
+			</header>
+		</div>
+	);
 }
 
 export default App;
